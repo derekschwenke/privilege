55e41ab536e67cf3a8d0169c4fc30b945d066166
Derek <derekschwenke@gmail.com>
2019-07-08 11:37:22 -0400

    Update Privilege.py
    added editing and output

--- Privilege.py
+++ Privilege.py
@@ -1,19 +1,16 @@
 from collections import defaultdict
 import sys
-import re
 
 """
-This Privilege reporting program scans 3 CSV input files and generates a report. 
+This Privilege reporting program scans 3 input files and generates a report. It allows the data to be edited and saved.
 Permissions are strings granting users privilege to perform a specific task on a resource, depending on their roles. 
 Permissions are additive and positive. 
 Future work will define the permission strings and group strings. 
 
-Output
-
 python 2.7  
 
 Usage:
-    python Privilege 
+    python Privilege [-interactive]
     
 Input:
 Three input files are required in CSV format in the working directory: users.txt, roles.txt and, permissions.txt.
@@ -37,7 +34,7 @@ ops, read, write
 admin, read, write, execute
 
 Output:
-A report of 6 tables.
+A report of 6 tables, and user focused tables.
 """
 
 
@@ -59,12 +56,12 @@ class User:
 
 
 class Privileges:
-    """ Privileges scans input files """
+    """ Privileges reporting """
 
     def readCSV(self, file):
         with open(file, 'r') as f:
             lines = f.read().splitlines()
-            return [[ field.strip() for field in line.split(',') ] for line in lines ]
+            return [[field.strip() for field in line.split(',')] for line in lines]
 
     def writeCSV(self, file, lines):
         with open(file, 'w') as f:
@@ -74,6 +71,20 @@ class Privileges:
         self.users_file = self.readCSV("users.txt")
         self.permissions_file = self.readCSV("permissions.txt")
         self.roles_file = self.readCSV("roles.txt")
+
+    def write(self):
+        self.writeCSV("users.out.txt", [[user.id, user.first, user.last] for user in sorted(self.userByID.values(), key=User.get_key)])
+        self.writeCSV("roles.out.txt", [[key] + sorted(self.usersHaveRole[key]) for key in sorted(self.usersHaveRole)])
+        self.writeCSV("permissions.out.txt", [[key] + sorted(self.permissionsForRole[key]) for key in sorted(self.permissionsForRole)])
+
+    def delUser(self, uid):
+        del self.userByID[uid]
+        for role, users in self.usersHaveRole.items():
+            users.discard(uid)
+
+    def delRole(self, role):
+        del self.permissionsForRole[role]
+        del self.usersHaveRole[role]
 
     def load(self):
         for v in self.users_file:  # [100, "Mel", "Marvin" ],
@@ -94,7 +105,6 @@ class Privileges:
                 if not self.userByID.has_key(uid):
                     print "Error: roles file contains new user ", uid
                     self.userByID[uid] = User(uid, "first_" + uid, "last_" + uid)
-
                 self.usersHaveRole[role].add(uid)  # add user to table of roles
                 self.userByID[uid].roles.add(role)  # add role to table of users
 
@@ -107,7 +117,6 @@ class Privileges:
                     print "Error: role '", role, "' missing from permission.txt file for user ", user
 
     def report(self):
-
         print "====== User Permissions"
         for u in sorted(self.userByID.values(), key=User.get_key):
             print "{:28} : {}".format(u, " ".join(sorted(u.role_permissions)))
@@ -128,22 +137,43 @@ class Privileges:
         for p in sorted(self.allPermissions):
             print "{:16} : {}".format(p, " ".join(sorted(
                 [r for r in self.permissionsForRole.keys() if p in self.permissionsForRole[r]])))
-
         print "\n============== User reports:"
         for u in sorted(self.userByID.values(), key=User.get_key):
-            print "{:28} : {}".format(u, " ".join(sorted(u.role_permissions)))
+            print "{:28}".format(u)
             for p in sorted(u.role_permissions):
-                print "    {:16} : {}".format(p, " ".join(sorted(  [r for r in u.roles if p in self.permissionsForRole[r]] )))
+                print "    {:16} : {}".format(p,
+                                              " ".join(sorted([r for r in u.roles if p in self.permissionsForRole[r]])))
 
     def scan(self):
-        line = ""
-        while line != "end" or line != "quit":
+        while True:
             line = raw_input('Command: ')
-            if not line:
+            if not line or line == "end" or line == "exit" or line == "quit":
                 break
             words = line.split(" ")
             if words[0] == "help":
-                print "quit?"
+                print "quit, read, write, report, create, add, delete."
+            elif words[0] == "report":
+                self.report()
+            elif words[0] == "write":
+                self.write()
+            elif line.startswith("create user"):
+                User(words[2], words[3], words[4])
+            elif line.startswith("delete user"):
+                self.delUser(words[2])
+            elif line.startswith("create role"):
+                self.permissionsForRole[words[2]] = line[3:]
+            elif line.startswith("delete role"):
+                self.delRole(words[2])
+            elif line.startswith("add role"):
+                self.usersHaveRole[words[2]] = self.usersHaveRole[words[2]] + set(line[3:])
+            elif line.startswith("delete role"):
+                self.usersHaveRole[words[2]] = self.usersHaveRole[words[2]].difference(set(line[3:]))
+            elif line.startswith("create permission"):
+                self.allPermissions.add(set(line[3:]))
+            elif line.startswith("delete permission"):
+                self.allPermissions.discard((words[2]))
+            else:
+                print "No command:", line
 
     def __init__(self):
         self.allPermissions = set()
@@ -154,11 +184,10 @@ class Privileges:
         self.read()
         self.load()
         self.report()
+        self.write()
+
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        # opts = sys.argv[1]
-        access = Privileges()
-        # access.scan()
-    else:
-        print "Usage: Privilege "
+    privileges = Privileges()
+    if len(sys.argv) > 1:
+        privileges.scan()
